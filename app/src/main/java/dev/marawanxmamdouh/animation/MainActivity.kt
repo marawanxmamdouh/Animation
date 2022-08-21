b992f54b4b8964981394a50786219f4ee52b28d9
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-21 19:15:22 +0200

    Rotate the Star

--- app/src/main/java/dev/marawanxmamdouh/animation/MainActivity.kt
+++ app/src/main/java/dev/marawanxmamdouh/animation/MainActivity.kt
@@ -1,6 +1,10 @@
 package dev.marawanxmamdouh.animation
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import dev.marawanxmamdouh.animation.databinding.ActivityMainBinding
 
@@ -12,8 +16,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater, null, false)
 
-
+        binding.rotateButton.setOnClickListener {
+            rotateButton()
+        }
 
         setContentView(binding.root)
+    }
+
+    private fun rotateButton() {
+        val animator = ObjectAnimator.ofFloat(binding.star, View.ROTATION, -360f, 0f)
+        animator.duration = 1000
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                binding.rotateButton.isEnabled = false
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                super.onAnimationEnd(animation)
+                binding.rotateButton.isEnabled = true
+            }
+        })
+        animator.start()
     }
 }

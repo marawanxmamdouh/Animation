e71f27a5b79d24f51fa27c1164c672886d8de230
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-21 18:58:55 +0200

    Create layout and add resources

--- app/src/main/java/dev/marawanxmamdouh/animation/MainActivity.kt
+++ app/src/main/java/dev/marawanxmamdouh/animation/MainActivity.kt
@@ -1,11 +1,19 @@
 package dev.marawanxmamdouh.animation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.marawanxmamdouh.animation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater, null, false)
+
+
+
+        setContentView(binding.root)
     }
 }

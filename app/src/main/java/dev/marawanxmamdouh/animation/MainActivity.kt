eaf5ea438932a52246e01d04773824a251ceab07
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-21 20:33:08 +0200

    Add shower animation

--- app/src/main/java/dev/marawanxmamdouh/animation/MainActivity.kt
+++ app/src/main/java/dev/marawanxmamdouh/animation/MainActivity.kt
@@ -1,13 +1,16 @@
 package dev.marawanxmamdouh.animation
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.ObjectAnimator
-import android.animation.PropertyValuesHolder
+import android.animation.*
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.LinearInterpolator
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
 import dev.marawanxmamdouh.animation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -36,6 +39,10 @@ class MainActivity : AppCompatActivity() {
 
         binding.colorizeButton.setOnClickListener {
             colorize()
+        }
+
+        binding.showerButton.setOnClickListener {
+            shower()
         }
 
         setContentView(binding.root)
@@ -88,6 +95,7 @@ class MainActivity : AppCompatActivity() {
         animator.start()
     }
 
+    @SuppressLint("ObjectAnimatorBinding")
     private fun colorize() {
         val animator = ObjectAnimator.ofArgb(
             binding.star.parent,
@@ -102,5 +110,64 @@ class MainActivity : AppCompatActivity() {
         animator.start()
     }
 
+    private fun shower() {
 
+        // Create a new star view in a random X position above the container.
+        // Make it rotateButton about its center as it falls to the bottom.
+
+        // Local variables we'll need in the code below
+        val star = binding.star
+        val container = star.parent as ViewGroup
+        val containerW = container.width
+        val containerH = container.height
+        var starW: Float = star.width.toFloat()
+        var starH: Float = star.height.toFloat()
+
+        // Create the new star (an ImageView holding our drawable) and add it to the container
+        val newStar = AppCompatImageView(this)
+        newStar.setImageResource(R.drawable.ic_star)
+        newStar.layoutParams = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.WRAP_CONTENT,
+            FrameLayout.LayoutParams.WRAP_CONTENT
+        )
+        container.addView(newStar)
+
+        // Scale the view randomly between 10-160% of its default size
+        newStar.scaleX = Math.random().toFloat() * 1.5f + .1f
+        newStar.scaleY = newStar.scaleX
+        starW *= newStar.scaleX
+        starH *= newStar.scaleY
+
+        // Position the view at a random place between the left and right edges of the container
+        newStar.translationX = Math.random().toFloat() * containerW - starW / 2
+
+        // Create an animator that moves the view from a starting position right about the container
+        // to an ending position right below the container. Set an accelerate interpolator to give
+        // it a gravity/falling feel
+        val mover = ObjectAnimator.ofFloat(newStar, View.TRANSLATION_Y, -starH, containerH + starH)
+        mover.interpolator = AccelerateInterpolator(1f)
+
+        // Create an animator to rotateButton the view around its center up to three times
+        val rotator = ObjectAnimator.ofFloat(
+            newStar, View.ROTATION,
+            (Math.random() * 1080).toFloat()
+        )
+        rotator.interpolator = LinearInterpolator()
+
+        // Use an AnimatorSet to play the falling and rotating animators in parallel for a duration
+        // of a half-second to two seconds
+        val set = AnimatorSet()
+        set.playTogether(mover, rotator)
+        set.duration = (Math.random() * 1500 + 500).toLong()
+
+        // When the animation is done, remove the created view from the container
+        set.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                container.removeView(newStar)
+            }
+        })
+
+        // Start the animation
+        set.start()
+    }
 }
